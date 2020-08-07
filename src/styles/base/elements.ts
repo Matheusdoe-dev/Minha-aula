c2180239313bdb8ac529708248ec79f63aea189c
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-08-06 21:56:05 -0300

    :lipstick: feat: global styles files created
    elements.ts, normalize.ts, reset.ts, colors.ts, gaps.ts

---
+++ src/styles/base/elements.ts
@@ -0,0 +1,69 @@
+import { createGlobalStyle } from "styled-components";
+
+const Elements = createGlobalStyle`
+    body {
+        font-family: Lato, Roboto, Helvetica, Arial, sans-serif !important;
+        color: var(--support-4);
+    }
+    
+    h1 {
+        font-weight: 900;
+    }
+    
+    h2,h3,h4 {
+        font-weight: 700;
+    }
+
+    h1, h2, h3, h4 {
+        text-transform: uppercase;
+        letter-spacing: .2px;
+        line-height: 1.15em;
+    }
+
+    h1 {
+        font-size: 3rem;
+        font-weight: 900;
+        
+        @media (max-witdh: 600px) {
+            & {
+                font-size: 2.25rem;
+            }
+        }
+    }
+
+    h2 {
+        font-size: 2.25rem;
+        @media (max-width: 600px) {
+        & {
+            font-size: 1.5rem;
+        }
+      }
+    }
+
+    h3 {
+        font-size: 1.5rem;
+    }
+
+    p, label, span, input::placeholder {
+        font-weight: 400;
+    }
+
+    p {
+        font-size: 1.25rem;
+        font-weight: 400rem;
+        line-height: 1.5em;
+    }
+
+    a, button {
+        font-size: 1.375;
+        letter-spacing: .3px;
+        text-transform: uppercase;
+    }
+
+    img {
+        display: block;
+        width: 100%;
+    }
+`;
+
+export default Elements;
---
+++ src/styles/settings/colors.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const Colors = createGlobalStyle`
+    :root {
+    --base: #227F84;
+    --base-2: #11676B;
+    --secondary-1: #E8DB3F;
+    --secondary-2: #CFC123;
+    --support-1: #FFFFFF;
+    --support-2: #F0F0F0;
+    --support-3: #D2D6D5;
+    --support-4: #322C2C;
+    }
+`;
+
+export default Colors;
