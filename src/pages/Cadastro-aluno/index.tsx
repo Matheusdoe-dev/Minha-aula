2d1158d84e74fe3fd0a30c3c2396801f727968e5
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-08-06 21:54:00 -0300

    :lipstick: feat: CadastroAluno page component created

---
+++ src/pages/Cadastro-aluno/index.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+// components
+import Header from "../../components/Header/index";
+import { Container, Grid } from "@material-ui/core";
+import Form from "../../components/Form/index";
+// styled-components
+import { CadastroAlunoContainer } from "./styles";
+import { Input } from "../../styles/objects/input";
+import { Button } from "../../styles/objects/button";
+// imgs
+import logoImg from "../../assets/logo.svg";
+
+const CadastroAluno = () => {
+  return (
+    <CadastroAlunoContainer>
+      <Header logo={logoImg} />
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item container sm={4}></Grid>
+          <Grid item container xs={12} sm={4}>
+            <Form title="CADASTRE-SE" subtitle="E APROVEITE!">
+              <label htmlFor="email">Nome completo</label>
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Digite seu nome completo"
+                required
+              />
+
+              <label htmlFor="email">Email</label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Digite o seu email"
+                required
+              />
+
+              <label htmlFor="email">Confirme seu e-mail</label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Confirme o seu email"
+                required
+              />
+
+              <label htmlFor="senha">Senha</label>
+              <Input
+                type="password"
+                name="senha"
+                id="senha"
+                placeholder="Digite sua senha"
+                required
+              />
+
+              <label htmlFor="senha">Confirme sua senha</label>
+              <Input
+                type="password"
+                name="senha"
+                id="senha"
+                placeholder="Confirme sua senha"
+                required
+              />
+
+              <Button
+                background="var(--secondary-1)"
+                color="var(--support-4)"
+                width="100%"
+                to="/aluno/painel"
+              >
+                Salvar Cadastro
+              </Button>
+            </Form>
+          </Grid>
+        </Grid>
+      </Container>
+    </CadastroAlunoContainer>
+  );
+};
+
+export default CadastroAluno;
---
+++ src/pages/Cadastro-aluno/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+// bg
+import loginBg from "../../assets/login-bg.jpg";
+
+export const CadastroAlunoContainer = styled.main`
+  & {
+    max-width: 100vw;
+    height: auto;
+    background: url(${loginBg});
+    background-size: contain;
+    background-repeat: repeat;
+    padding-bottom: var(--gap-xl);
+
+    span {
+      display: block;
+      text-align: center;
+      margin: var(--gap-sm) auto;
+    }
+
+    h4 {
+      margin-bottom: var(--gap-md);
+
+      &::after {
+        content: "";
+        display: block;
+        width: 46px;
+        border: 1px solid var(--support-1);
+        opacity: 20%;
+        margin: calc(var(--gap) / 2) 0 var(--gap-md) 0;
+      }
+    }
+  }
+`;
