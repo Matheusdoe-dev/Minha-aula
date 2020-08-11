f34e65fd479ea83491e48d1e2c1874e257cc41e7
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-08-11 10:46:45 -0300

    :lipstick: feat: Select component created

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,37 @@
+import React, { SelectHTMLAttributes } from "react";
+// styles
+import { SelectContainer } from "./styles";
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const SelectInput: React.FC<SelectProps> = ({
+  label,
+  name,
+  options,
+  ...rest
+}) => {
+  return (
+    <SelectContainer>
+      <label htmlFor={name}>{label}</label>
+      <select name={name} id={name} {...rest}>
+        <option value="" disabled hidden>
+          Selecione uma opção
+        </option>
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </SelectContainer>
+  );
+};
+
+export default SelectInput;
---
+++ src/components/Select/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+interface Props {
+  width?: string;
+  color?: string;
+  background?: string;
+}
+
+export const SelectContainer = styled.div`
+  & {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      margin-bottom: var(--gap);
+    }
+
+    select {
+      background: ${(props: Props) =>
+        props.width ? props.width : `var(--support-1)`};
+      opacity: 0.65;
+      border-radius: 5px;
+      width: ${(props: Props) => (props.width ? props.width : `100%`)};
+      color: ${(props: Props) =>
+        props.width ? props.width : `var(--support-4)`};
+      border: none;
+      padding: var(--gap);
+      transition: 0.2s;
+      box-sizing: border-box;
+
+      &:focus {
+        outline: none;
+        filter: brightness(75%);
+        transition: 0.2s;
+      }
+    }
+  }
+`;
