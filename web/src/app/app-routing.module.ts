9e4607349b56d92badceae3cbaf96ccee3a78740
matheusdoedev <matheusdoe.dev@gmail.com>
2021-03-15 17:53:58 -0300

    :construction: chore: new routes
    index and user-login

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { IndexPageComponent } from './views/index-page/index-page.component';
+import { IndexViewComponent } from './views/index/index-view.component';
+import { UserLoginViewComponent } from './modules/users/user-login/user-login-view.component';
 
 const routes: Routes = [
-  { path: '', component: IndexPageComponent, pathMatch: 'full' },
+  { path: 'user/login', component: UserLoginViewComponent },
+  { path: '', component: IndexViewComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '' },
 ];
 
