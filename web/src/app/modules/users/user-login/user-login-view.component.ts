eb475fc2f219ad2f2a13be2a2395c4da4e10e77e
matheusdoedev <matheusdoe.dev@gmail.com>
2021-03-15 17:53:14 -0300

    :construction: feat: user-login-view component

---
+++ web/src/app/modules/users/user-login/user-login-view.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './user-login-view.component.html',
+  styleUrls: ['./user-login-view.component.scss'],
+})
+export class UserLoginViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
