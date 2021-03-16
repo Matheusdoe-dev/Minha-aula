53fd6e45d98ef8374627e24d1ba953d4306fb521
matheusdoedev <matheusdoe.dev@gmail.com>
2021-03-16 16:31:52 -0300

    :construction: chore: FormsModule and InputComponent added
    FormsModule on imports and InputComponent on declarations and exports

--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from './button/button.component';
 import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [ButtonComponent, HeaderComponent],
+  declarations: [ButtonComponent, HeaderComponent, InputComponent],
+  imports: [CommonModule, FormsModule, RouterModule],
+  exports: [ButtonComponent, HeaderComponent, InputComponent, FormsModule],
 })
 export class SharedModule {}
