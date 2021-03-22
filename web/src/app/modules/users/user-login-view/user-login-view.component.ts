b6e016219a2ebbff565be2819aa2cabb02ada9bb
matheusdoedev <matheusdoe.dev@gmail.com>
2021-03-22 20:38:59 -0300

    :construction: chore: user-login-view styled

--- web/src/app/modules/users/user-login-view/user-login-view.component.ts
+++ web/src/app/modules/users/user-login-view/user-login-view.component.ts
@@ -1,13 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { UsersService } from 'src/app/services/users.service';
+
+import { LoginData } from './user-login-view.interface';
 
 @Component({
   templateUrl: './user-login-view.component.html',
   styleUrls: ['./user-login-view.component.scss'],
 })
-export class UserLoginViewComponent implements OnInit {
-  constructor() {
+export class UserLoginViewComponent {
+  login: LoginData = { email: '', senha: '' };
+
+  constructor(private _usersService: UsersService) {
     document.title = 'Login - Minha Aula';
   }
 
-  ngOnInit(): void {}
+  onValueChange(event: { value: string; data: string }): void {
+    this.login = { ...this.login, [event.data]: event.value };
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log('Hello World', form.valid, form);
+
+    this._usersService.postAuthentication(this.login).subscribe({
+      next: (r) => console.log(r),
+      error: (err) => console.log(err),
+    });
+  }
 }
